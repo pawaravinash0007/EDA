704083f0428eb0138151bd30ddcac46ce4f1d800
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-05-29 23:14:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("Wine Data Analysis")
